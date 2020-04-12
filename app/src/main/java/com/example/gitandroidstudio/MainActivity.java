303ae9f56a5bfe5e08e2c2dd67475b45e99dd750
Aliasgar Nasir <nasiraliasgar@gmail.com>
2020-04-13 01:12:17 +0530

    Main Activity Committed

--- app/src/main/java/com/example/gitandroidstudio/MainActivity.java
+++ app/src/main/java/com/example/gitandroidstudio/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("TAG","MESSAGE");
+
+        Log.i("TAG","MESSAGE");
     }
 }
